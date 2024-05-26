4ea7a50449fa561bd492707379024c2962375c8a
Ahmed Naeem <Ahmed.Naeem@stewart.com>
2024-05-26 18:38:06 +0500

    added backend project

---
+++ TruSec.Backend/TruSec.Backend/Hubs/ExpressionHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TruSec.Backend.Interfaces;
+
+namespace TruSec.Backend.Hubs
+{
+    public class ExpressionHub : Hub<IExpressionsHub>
+    {
+        public async Task SendExpression(string message)
+        {
+            await Clients.All.SendExpression(message);
+        }
+    }
+}
