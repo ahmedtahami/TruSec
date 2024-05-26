b3e13fc998831b69a3a2d01d673627e5ddc376aa
Ahmed Naeem <Ahmed.Naeem@stewart.com>
2024-05-26 20:05:59 +0500

    added entites

---
+++ TruSec.Backend/TruSec.DAL/Entities/TruckDataLog.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TruSec.DAL.Entities
+{
+    public class TruckDataLog
+    {
+        public int Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string DriverExpression { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int TruckId { get; set; }
+        [ForeignKey(nameof(TruckId))]
+        public Truck Truck { get; set; }
+        public double SpeedKPH { get; set; }
+    }
+}
