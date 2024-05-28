ded3fb3b3e930516c7e52972e5e7de4544c225ac
ahmedtahami <ahmedtahami@yahoo.com>
2024-05-28 18:19:14 +0500

    added maps on dashboard

--- TruSec.Backend/TruSec.Backend/Hubs/ExpressionHub.cs
+++ TruSec.Backend/TruSec.Backend/Hubs/ExpressionHub.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using TruSec.Backend.Controllers;
 using TruSec.Backend.Interfaces;
 
 namespace TruSec.Backend.Hubs
 {
     public class ExpressionHub : Hub<IExpressionsHub>
     {
-        public async Task SendExpression(string message)
+        public async Task SendExpression(ExpressionData data)
         {
-            await Clients.All.SendExpression(message);
+            await Clients.All.SendExpression(data);
         }
     }
 }
--- TruSec.WebClient/src/app/pages/main.component.ts
+++ TruSec.WebClient/src/app/pages/main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SignalRService } from '../services/signalR.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,13 +9,41 @@ import { SignalRService } from '../services/signalR.service';
 })
 export class MainComponent {
   public expression!: string;
-
+  private map!: L.Map;
+  private marker!: L.Marker;
+  public latitude: number = 2233.2222;
+  public longitude: number = 3.44;
   constructor(private signalRService: SignalRService) { }
 
   ngOnInit() {
+    this.initMap();
     this.signalRService.startConnection();
-    this.signalRService.addTransferChartDataListener((data: string) => {
-      this.expression = data;
+    this.signalRService.addTransferDataListener((data: any) => {
+      debugger;
+      this.expression = data.expression ?? '';
+      this.latitude = data.latitude ?? NaN;
+      this.longitude = data.longitude ?? NaN;
+      this.updateMarker();
     });
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([0, 0], 2); // Initial map view
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+    }).addTo(this.map);
+
+    this.marker = L.marker([0, 0]).addTo(this.map)
+      .bindPopup('Truck Location');
+  }
+
+  private updateMarker(): void {
+    if (this.marker) {
+      this.marker.setLatLng([this.latitude, this.longitude]);
+      this.map.setView([this.latitude, this.longitude], 13);
+      this.marker.getPopup()?.setContent(`Truck Location<br>Expression: ${this.expression}`);
+      this.marker.openPopup();
+    }
   }
 }
